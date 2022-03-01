cb7a43d9de9943fce7652989f6740d2b5afac815
maho10 <majix.majito@gmail.com>
2022-03-01 11:26:05 -0600

    DefineVrFn created

---
+++ src/DefineFnVr.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class DefineFnVr {
+
+    private HashMap<String, String[]> functions;
+    private HashMap<String, String> variables;
+
+    public void createFn(String name, ArrayList<String> code){}
+    public ArrayList<String> excecuteFn(String name){
+        return null;
+    }
+    public void createVr (String name, String value){}
+    public String showVr(String name){
+        return null;
+    }
+    public HashMap<String, String> getVr{}
+    public HashMap<String, String[]> getFN{}
+    public int getSize{
+        return 0;
+    }
+    public ArrayList<String> getBody(String name){}
+    public DefineFnVr() {}
+}
