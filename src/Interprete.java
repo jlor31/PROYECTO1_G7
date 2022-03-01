732dc73a50a24d079461efe87f2844ae73481d5a
maho10 <majix.majito@gmail.com>
2022-03-01 11:40:15 -0600

    Create Interprete.java

---
+++ src/Interprete.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+
+public class Interprete {
+
+    private ArrayList<String> code;
+
+    public void save(){};
+    public void test(){};
+    public void excecute(){};
+
+}
