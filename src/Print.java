863e1d1d03a25566c0c706ea2890a8d0a7b28a01
maho10 <majix.majito@gmail.com>
2022-03-01 11:36:04 -0600

    Print file

---
+++ src/Print.java
@@ -0,0 +1,11 @@
+public class Print {
+
+    public String print(String code){
+        return null;
+    }
+    public String[] read(String code){
+        return null;
+    }
+    public Print(){}
+}
+
