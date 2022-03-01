033d23a01afc88ae440a709199ffddf2a7d6cc74
maho10 <majix.majito@gmail.com>
2022-03-01 11:38:18 -0600

    Calcualdora

---
+++ src/Calculator.java
@@ -0,0 +1,13 @@
+import java.util.HashMap;
+
+public class Calculator {
+
+    private String num;
+    private String operator;
+    private HashMap<String, String> variables;
+
+    public String operate(String operation){
+        return null;
+    }
+    public Calculator(){}
+}
