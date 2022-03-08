06b49a53fcea4c7bbe3493f66b032e04db60bc48
maho10 <majix.majito@gmail.com>
2022-03-08 11:41:19 -0600

    Definir funciones y variables

--- src/DefineFnVr.java
+++ src/DefineFnVr.java
@@ -1,30 +1,51 @@
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Map;
 
 public class DefineFnVr {
 
-    private HashMap<String, String[]> functions;
+    private HashMap<String, ArrayList<String>> functions;
     private HashMap<String, String> variables;
 
-    public void createFn(String name, ArrayList<String> code){}
-    public ArrayList<String> excecuteFn(String name){
-        return null;
+    public void createFn(String name, ArrayList<String> code){
+        functions.put(name, code);
+
     }
-    public void createVr (String name, String value){}
+    public ArrayList<String> excecuteFn(String name){
+        ArrayList<String> result;
+        result = null;
+        for(Map.Entry<String, ArrayList<String>> indivFn:functions.entrySet()){
+            if(indivFn.getKey().equals(name)){
+                result = indivFn.getValue();
+            }
+        }
+        return result;
+    }
+    public void createVr (String name, String value){
+        variables.put(name, value);
+    }
     public String showVr(String name){
-        return null;
+        String value= "";
+        for (Map.Entry<String, String> indivVr: variables.entrySet()){
+            if(indivVr.getKey().equals(name)) value = indivVr.getValue();
+        }
+        if(value.equals("")) return null;
+        else return value;
     }
     public HashMap<String, String> getVr (){
-        return null;
+        return variables;
     }
-    public HashMap<String, String[]> getFN (){
-        return null;
-    }
-    public int getSize(){
-        return 0;
+    public HashMap<String, ArrayList<String>> getFN (){
+        return functions;
     }
     public ArrayList<String> getBody(String name){
-        return null;
+        return functions.get(name);
     }
-    public DefineFnVr() {}
+    public String getName(String name){
+        return variables.get(name);
+    }
+    public DefineFnVr() {
+        functions = new HashMap<String, ArrayList<String>>();
+        variables = new HashMap<String, String>();
+    }
 }
